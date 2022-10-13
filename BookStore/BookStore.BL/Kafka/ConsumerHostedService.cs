5f558ceccde3bf6cd6980868ce72ebd590d67d1c
mchausheva <maya.chausheva@yahoo.com>
2022-10-13 10:58:34 +0300

    Kafka

--- BookStore/BookStore.BL/Kafka/ConsumerHostedService.cs
+++ BookStore/BookStore.BL/Kafka/ConsumerHostedService.cs
@@ -11,7 +11,8 @@ namespace BookStore.BL.Kafka
         ILogger<ConsumerHostedService<TKey, TValue>> _logger;
         private IOptions<KafkaSettings> _kafkaSettings;
         private readonly string _topicName;
-        private readonly ConsumerConfig _consumerConfig;    
+        private readonly ConsumerConfig _consumerConfig;
+        private readonly IConsumer<TKey, TValue> _consumerBuilder;
         public ConsumerHostedService(IOptions<KafkaSettings> kafkaSettings, ILogger<ConsumerHostedService<TKey, TValue>> logger)
         {
             _kafkaSettings = kafkaSettings;
@@ -23,25 +24,24 @@ namespace BookStore.BL.Kafka
                 AutoOffsetReset = (AutoOffsetReset?)_kafkaSettings.Value.AutoOffsetReset,
                 GroupId = _kafkaSettings.Value.GroupId
             };
+            _consumerBuilder = new ConsumerBuilder<TKey, TValue>(_consumerConfig)
+                .SetKeyDeserializer(new MsgPackDeserializer<TKey>())
+                .SetValueDeserializer(new MsgPackDeserializer<TValue>())
+                .Build();
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
 
             _logger.LogInformation($"Start -> {nameof(ConsumerHostedService<TKey, TValue>)}");
-               
-            var consumer = new ConsumerBuilder<TKey, TValue>(_consumerConfig)
-                .SetKeyDeserializer(new MsgPackDeserializer<TKey>())
-                .SetValueDeserializer(new MsgPackDeserializer<TValue>())
-                .Build();
 
-            consumer.Subscribe(_topicName);
+            _consumerBuilder.Subscribe(_topicName);
 
             var t = Task.Run(() =>
             {
                 while (true)
                 {
-                    var cr = consumer.Consume();
+                    var cr = _consumerBuilder.Consume();
 
                     Console.WriteLine($"Received msg with Key: [{cr.Message.Key}] & Value: {cr.Message.Value}");
                 }
@@ -54,6 +54,8 @@ namespace BookStore.BL.Kafka
         public Task StopAsync(CancellationToken cancellationToken)
         {
             _logger.LogInformation($"Stopping -> {nameof(ConsumerHostedService<TKey, TValue>)}");
+
+            _consumerBuilder.Dispose();
 
             return Task.CompletedTask;
         }
